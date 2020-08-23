91723e30e8b80001a8e371146a956e043b6a43b2
shankareaga <69393623+shankareaga@users.noreply.github.com>
2020-08-23 13:58:39 +0100

    Add files via upload

---
+++ area_name_main.py
@@ -0,0 +1,9 @@
+import json
+def calculate_area(base, height):
+    print("_name_ :", __name__)
+    return 1/2*(base*height)
+
+if __name__ == "__main__" :
+    print("I am in area.py")
+    a = calculate_area(10, 20)
+    print("Area :", a)
